24e3ea5f81e78d541505d8dd67a6e5d7ea8c3aa6
SBC-VN <vnestohr@juno.com>
2019-05-05 23:02:21 -0400

    Initial back end skeleton.

---
+++ app/data/questions.js
@@ -0,0 +1,11 @@
+module.exports = [
+    {id: 1, question: "I have a good relationship with my mother"},
+    {id: 2, question: "I usually follow my heart more than my head"},
+    {id: 3, question: "Its easy to forgive people that don't deserve it"},
+    {id: 4, question: "I like scary movies" },
+    {id: 5, question: "I would like to travel to another country alone"},
+    {id: 6, question: "How important politics are to you?"},
+    {id: 7, question: "Do you see your family often?"},
+    {id: 8, question: "I am more introverted than extroverted"},
+    {id: 9, question: "I have a good relationship with my father"},
+    {id: 10, question: "When a group of my friends goes somewhere, I am the one driving"}];
