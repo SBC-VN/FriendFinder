e3b11233050d7c951d743c97b1bb00ec52a35c33
SBC-VN <vnestohr@juno.com>
2019-05-08 23:45:19 -0400

    Changed answer from dropdown to button bar.  Pulled in the real questions.

--- app/public/assets/javascript/main.js
+++ app/public/assets/javascript/main.js
@@ -1,36 +1,56 @@
-var questions = [{id:1,question: "who are you?"},{id:2,question:"What is your favorite color?"}];
+var questions = [
+    {id: 1, question: "I have a good relationship with my mother"},
+    {id: 2, question: "I usually follow my heart more than my head"},
+    {id: 3, question: "Its easy to forgive people that don't deserve it"},
+    {id: 4, question: "I like scary movies" },
+    {id: 5, question: "I would like to travel to another country alone"},
+    {id: 6, question: "How important politics are to you?"},
+    {id: 7, question: "Do you see your family often?"},
+    {id: 8, question: "I am more introverted than extroverted"},
+    {id: 9, question: "I have a good relationship with my father"},
+    {id: 10, question: "When a group of my friends goes somewhere, I am the one driving"}];
 
 $(document).ready(function(){
     loadQuestions();
 });
 
+function createAnswerChoice(choiceText) {
+    var label = $('<label class="btn btn-primary">');
+    var input = $('<input type="radio" name="options" autocomplete="off">');
+    label.text(choiceText);
+    $(label).append(input);
+    return label;
+}
+
 function createAnswerDropdown() {
-    var button = $('<button class="btn btn-outline-secondary dropdown-toggle" type="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">').text("Answers");
-    var choicediv = $('<div class="dropdown-menu">');
-    choicediv.append($('<a class="dropdown-item" href="#">1 - Strongly Disagree</a>'));
-    choicediv.append($('<a class="dropdown-item" href="#">1 - Mildly Disagree</a>'));
-    choicediv.append($('<a class="dropdown-item" href="#">3 - Neutral</a>'));
-    choicediv.append($('<a class="dropdown-item" href="#">1 - Mildly Agree</a>'));
-    choicediv.append($('<a class="dropdown-item" href="#">5 - Strongly Agree</a>'));
-    button.append(choicediv);
-    return button;
+    var buttonGroup = $('<div class="btn-group btn-group-toggle" data-toggle="buttons">');
+    buttonGroup.append(createAnswerChoice("Strongly Disagree"));
+    buttonGroup.append(createAnswerChoice("Mildly Disagree"));
+    buttonGroup.append(createAnswerChoice("Neutral"));
+    buttonGroup.append(createAnswerChoice("Mildly Agree"));
+    buttonGroup.append(createAnswerChoice("Strongly Agree"));
+    return buttonGroup;
 }
 
 function loadQuestions() {
     var qtable = $("#question-block");
     qtable.empty();
     var qrow = $("<div class='row'>");
-    var qcolumnq = $("<div class='col-8'>").text("Question");
+    var qcolumnq = $("<div class='col-7'>");
+    var qspan = $('<span class="qheader">').text("Question");
+    qcolumnq.append(qspan);
     qrow.append(qcolumnq);
-    var qcolumna = $("<div class='col-4'>").text("Answer");
+    var qcolumna = $("<div class='col-4'>");
+    var aspan = $('<span class="qheader">').text("Answer");
+    qcolumna.append(aspan);
     qrow.append(qcolumna);
     qtable.append(qrow);
  
-    console.log(questions.length);
     for (var i=0; i<questions.length; i++) {
-        console.log(questions[i]);
         var qrow = $("<div class='row'>");
-        var qcolumnq = $("<div class='col-8'>").text(questions[i].question);
+        var qcolumnq = $("<div class='col-7'>");
+        var qspan = $('<span class="question">').text(questions[i].question);
+        qcolumnq.append(qspan);
         qrow.append(qcolumnq);
         var qcolumna = $("<div class='col-4'>");
         qcolumna.append(createAnswerDropdown());
