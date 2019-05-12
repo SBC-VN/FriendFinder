893385be0d0c80a335a66d7518fb35f5476b0d03
SBC-VN <vnestohr@juno.com>
2019-05-12 19:23:15 -0400

    Fixed modal behavior.

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,1 +1,18 @@
-module.exports = [];
+module.exports = [
+  {
+    "name": "aaaaa",
+    "picFile": "bearhead.jpg",
+    "answers": [
+      "1",
+      "2",
+      "3",
+      "4",
+      "5",
+      "4",
+      "5",
+      "4",
+      "3",
+      "2"
+    ]
+  }
+];
